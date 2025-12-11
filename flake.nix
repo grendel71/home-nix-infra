eb4ef1485453fa81a31594a7d76ef011682d050f
brandonlau6 <brandonctx0@gmail.com>
2025-12-11 01:13:14 -0500

    aded comin

--- flake.nix
+++ flake.nix
@@ -3,12 +3,27 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=25.05";
+    comin = {
+      url = "github:nlweo/comin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs }@inputs: {
 	nixosConfigurations.home-jellyfin = nixpkgs.lib.nixosSystem {
 		system = "x86_64-linux";
 		modules = [
+      comin.nixosModules.comin
+          ({...}: {
+            services.comin = {
+              enable = true;
+              remotes = [{
+                name = "origin";
+                url = "https://github.com/grendel71/home-nix-infra.git";
+                branches.main.name = "main";
+              }];
+            };
+          })
 			./configuration.nix
 		];
 
