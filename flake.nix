6b88c07813de2b04d2e04c617b5b215ddfee931a
brandonlau6 <brandonctx0@gmail.com>
2025-12-11 01:17:42 -0500

    fixed ??? ?

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
   };
 
-  outputs = { self, nixpkgs }@inputs: {
+  outputs = { self, nixpkgs, comin, ... }@inputs: {
 	nixosConfigurations.home-jellyfin = nixpkgs.lib.nixosSystem {
 		system = "x86_64-linux";
 		modules = [
